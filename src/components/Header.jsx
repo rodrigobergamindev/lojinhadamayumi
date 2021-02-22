b5952ec7cb34f64c3490567175e5af13b1d67299
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-02-22 10:30:00 -0300

    catalogo de roupas att

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
     const urlInsta = 'https://www.instagram.com/lojinha_da_mayumi/'
     const urlCatalogoACO = 'https://drive.google.com/file/d/1AFcbE7RGP4XALwh0DYl3MNxwxJrgZrkR/view?usp=sharing'
     const urlCatalogoPRATA = 'https://drive.google.com/file/d/1lpEPu9i02b75BkZebPlw9sLShA484Brl/view?usp=sharing'
-    const urlCatalogoROUPA = 'https://drive.google.com/file/d/1wmLx6eWKOSlaUwF7LJF7FBVJhzAV1F_m/view?usp=sharing'
+    const urlCatalogoROUPA = 'https://drive.google.com/file/d/1x-xN60fPiSNUAunsi9xWMep5kKpkBzHY/view?usp=sharing'
     const urlCatalogoARGOLA = 'https://drive.google.com/file/d/1oikyEFrQDoqehJ8dKRUngY1Avu53r0nb/view?usp=sharing'
     const urlWhatsApp = 'https://api.whatsapp.com/send?phone=551129639226&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20as%20pe%C3%A7as%20da%20loja'
     const urlFace = 'https://www.facebook.com/lojinhadamayumi/'
