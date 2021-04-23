e30122c7733ebabe9efbd276d2106121e57b2976
Rodrigo <rodrigobergamindev@gmail.com>
2021-04-22 22:36:45 -0300

    att 22/04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,10 @@ const useStyles = makeStyles({
 export default function Header() {
     const classes = useStyles();
     const urlInsta = 'https://www.instagram.com/lojinha_da_mayumi/'
-    const urlCatalogoACO = 'https://drive.google.com/file/d/1qU2EHmmRTaa2Ry9Y2YkTg0AKQ1LMqw72/view?usp=sharing'
-    const urlCatalogoPRATA = 'https://drive.google.com/file/d/1z8Q32gfDhyozjfY8goIigLA-Gr7yw5Hj/view?usp=sharing'
+    const urlCatalogoACO = 'https://drive.google.com/file/d/1WSKPF7S3IKMgV5LlaGH7RPLLL4tHm4h-/view?usp=sharing'
+    const urlCatalogoPRATA = 'https://drive.google.com/file/d/1ZIsEEdR5StrEl7ZqK4ArE4VIh-QmlM0N/view?usp=sharing'
     const urlCatalogoARGOLA = 'https://drive.google.com/file/d/14Qts8zYV_0S_lbeXEzH02PD1iXfdXQZm/view?usp=sharing'
-    const urlCatalogoNIQUEL = 'https://drive.google.com/file/d/1XhvnZYioEl-cDv3uae9afeu5IP2VZAj4/view?usp=sharing'
+    const urlCatalogoNIQUEL = 'https://drive.google.com/file/d/1PZA7WR6uw73KAsSQUmebq7FtP-UzAtXT/view?usp=sharing'
     const urlWhatsApp = 'https://api.whatsapp.com/send?phone=551129639226&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20as%20pe%C3%A7as%20da%20loja'
     const urlFace = 'https://www.facebook.com/lojinhadamayumi/'
 
